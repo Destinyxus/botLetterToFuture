a2533ad8bda12fbfe5cf5b7c94811e27189e87c7
Vladimir <vldmrbusiness@gmail.com>
2023-03-25 04:18:09 +0300

    validation1.0

--- cmd/letterToFuture/main.go
+++ cmd/letterToFuture/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,7 +10,10 @@ import (
 
 	"LetterToFuture/internal/apiserver"
 	"LetterToFuture/internal/model"
+	"LetterToFuture/internal/telegram"
 )
+
+const MAX_MESSAGE_LIMIT = 4095
 
 func main() {
 	server := apiserver.NewAPIServer()
@@ -36,34 +40,67 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
-	var goletter bool = false
+	commands := telegram.NewCommands()
+
+	model2 := model.TemporaryModel()
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
+		if update.Message.Text != "" { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			if update.Message.Command() == "start" {
-				goletter = false
+			switch update.Message.Command() {
+			case "start":
+				commands.CommandMode(update.Message.Command())
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, "+update.Message.From.UserName+"! Это бот \"Письма в будущее\". "+
 					"Чтобы ознакомиться поподробнее с тем, что я умею, вызовете комманду /help.")
 				bot.Send(msg)
-
-			} else if update.Message.Command() == "help" {
-				goletter = false
+			case "help":
+				commands.CommandMode(update.Message.Command())
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я отправляю письма в будущее. Для того чтобы начать вызовете команду /goletter")
 				bot.Send(msg)
-			} else if update.Message.Command() == "goletter" {
+			case "goletter":
+				commands.CommandMode(update.Message.Command())
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Отправьте текст своего письма:")
 				bot.Send(msg)
-				goletter = true
-				continue
-			}
-			if goletter == true {
-				textMsg := update.Message.Text
+			default:
+				if commands.Start == true {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Нажмите /help")
+					bot.Send(msg)
+				}
+				if commands.Help == true {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Нажмите /goletter")
+					bot.Send(msg)
+				}
+				if commands.Goletter == true {
+					textMsg := update.Message.Text
+					if len(textMsg) > MAX_MESSAGE_LIMIT {
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Извини, я не могу сохранить такое большое письмо. Сократите его и попробуйте еще раз!")
+						bot.Send(msg)
+						continue
+					} else {
+						if model2.Letter == "" {
+							model2.Letter = update.Message.Text
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Теперь введите вашу почту:")
 
-				server.Store.CreateALetter(model.NewModel(textMsg, "2222-11-11", ""))
-			}
+							bot.Send(msg)
+						} else if model2.Email == "" {
+							model2.Email = update.Message.Text
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Теперь введите дату:")
+							bot.Send(msg)
+						} else if model2.Date == "" {
+							model2.Date = update.Message.Text
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Спасибо!")
+							bot.Send(msg)
+							fmt.Println(model2)
+						}
+						//server.Store.CreateALetter(model.NewModel("dsfafdasfasdfasd", "2222-11-11", textMsg))
+					}
 
+				}
+			}
+		} else {
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "К сожалению, я принимаю только текстовый формат."+
+				" Нажмите /help"))
 		}
 	}
+
 }
--- internal/model/model.go
+++ internal/model/model.go
@@ -10,3 +10,7 @@ type Model struct {
 func NewModel(email string, date string, letter string) *Model {
 	return &Model{Email: email, Date: date, Letter: letter}
 }
+
+func TemporaryModel() *Model {
+	return &Model{}
+}
